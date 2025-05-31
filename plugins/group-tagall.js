add2ea3332819aaca1c6ad7bc37b1e88f3b9239d
alexbotsx <toscren0@gmail.com>
2025-05-31 18:58:11 -0500

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -4,11 +4,11 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `ＭｉｃｈｉＢｏｔ ${pesan}`
+let oi = `𝙂𝙚𝙧𝙞𝙈𝙞𝙖𝙪 𝙩𝙚 𝙞𝙣𝙫𝙤𝙘𝙖 𝙥𝙚𝙧𝙧𝙚🫦 ${pesan}`
 let teks = `*𝘼𝙘𝙩𝙞𝙫𝙖𝙩𝙚 𝙥𝙡𝙖𝙣𝙩𝙖 𝙙𝙚 𝙢𝙧𝙙𝙖🌱🫦*\n${oi}\n\n*＠ｃｅｏｇｅｒｉｐｉｕｍ．ｄｚｎ🫶🏻*\n`
 for (let mem of participants) {
-teks += `🍔🍟ᝰ.ᐟ @${mem.id.split('@')[0]}\n`}
-teks += `MichiBot`
+teks += `🪄✨ᝰ.ᐟ @${mem.id.split('@')[0]}\n`}
+teks += `GeriMiau`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) } )
 }
 handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
