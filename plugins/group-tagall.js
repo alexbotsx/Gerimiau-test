2d5cc90170e15c4c38d262d10145ead14210981f
alexbotsx <toscren0@gmail.com>
2025-05-31 19:12:41 -0500

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,17 +1,19 @@
 let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
-if (!(isAdmin || isOwner)) {
-global.dfail('admin', m, conn)
-throw false
-}
-let pesan = args.join` `
-let teks = `𝙂𝙚𝙧𝙞𝙈𝙞𝙖𝙪 𝙩𝙚 𝙞𝙣𝙫𝙤𝙘𝙖 𝙥𝙚𝙧𝙧𝙚🫦*\n${oi}\n\n
-for (let mem of participants) {
-teks += `🪄✨ @${mem.id.split('@')[0]}\n`}
-teks += `GeriMiau`
-conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) } )
-}
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
-handler.admin = true
-handler.group = true
-handler.botAdmin = true
-export default handler
+  if (!(isAdmin || isOwner)) {
+  global.dfail('admin', m, conn)
+  throw false
+  }
+  let pesan = args.join` `
+  let oi = `𝙂𝙚𝙧𝙞𝙈𝙞𝙖𝙪 𝙩𝙚 𝙞𝙣𝙫𝙤𝙘𝙖 𝙥𝙚𝙧𝙧𝙚🫦 `${pesan}`
+  let teks = `👑@𝔠𝔢𝔬𝔤𝔢𝔯𝔦𝔭𝔦𝔲𝔪.𝔡𝔷𝔫`
+  for (let mem of participants) {
+  teks += `🪄✨ @${mem.id.split('@')[0]}\n`}
+  teks += `GeriMiau`
+  conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+  }
+  handler.help = ['tagall <mesaje>','invocar <mesaje>']
+  handler.tags = ['group']
+  handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+  handler.admin = true
+  handler.group = true
+  export default handler
